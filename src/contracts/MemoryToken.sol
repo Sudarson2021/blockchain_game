91f1b8e9463a2aa6a060ac15e300d86641636fab
Sudarson Karmaker <sudarsonkarmaker@yahoo.com>
2023-05-08 20:32:46 +0100

    Blockchain gaming dapps

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,7 +2,15 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-contract MemoryToken is ERC721Full {
-  // Code goes here...
+  contract MemoryToken is ERC721Full {
+  constructor() ERC721Full("Memory Token","MEMORY") public {
 
+  }
+
+  function mint(address _to, string memory _tokenURI) public returns(bool) {
+    uint _tokenID = totalSupply().add(1);
+    _mint(_to, _tokenID);
+    _setTokenURI(_tokenID, _tokenURI);
+    return true;
+  }
 }
